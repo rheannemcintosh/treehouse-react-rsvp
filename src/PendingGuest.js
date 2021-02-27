bc2851a815334234826a99fcb7557949c911a3ac
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-27 14:40:18 +0000

    Refactor PendingGuest.js

--- src/PendingGuest.js
+++ src/PendingGuest.js
@@ -1,6 +1,19 @@
+/**
+ * PendingGuest.js
+ * 
+ * @file Manages the Pending Guest Component
+ * @author Rheanne McIntosh <rheanne.mcintosh@outlook.com>
+ */
+
+// Import Statements
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Create the PendingGuest Component
+ * 
+ * @param {*} props 
+ */
 const PendingGuest = props => {
     if (props.name) {
         return (
@@ -12,8 +25,10 @@ const PendingGuest = props => {
     return null;
 };
 
+// Set the PendingGuest Prop Types
 PendingGuest.propTypes = {
     name: PropTypes.string.isRequired,
 };
 
+// Export the PendingGuest Component
 export default PendingGuest;
