14f502853294178ad0e3dc1e76b452559a5d4418
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-02-27 14:39:54 +0000

    Refactor GuestList.js

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,27 +1,45 @@
+/**
+ * GuestList.js
+ * 
+ * @file Manages the GuestList Component
+ * @author Rheanne McIntosh <rheanne.mcintosh@outlook.com>
+ */
+
+// Import Statements
 import React from 'react';
+import PropTypes from 'prop-types';
 import Guest from './Guest';
 import PendingGuest from './PendingGuest';
-import PropTypes from 'prop-types';
 
+/**
+ * Create the GuestList Component
+ * 
+ * @param {*} props 
+ */
 const GuestList = props =>
-<ul>
-  <PendingGuest name={props.pendingGuest} />
-{props.guests
-  .filter(guest => !props.isFiltered || guest.isConfirmed)
-  .map((guest, index) =>
-  <Guest 
-    key={index}
-    name={guest.name}
-    isConfirmed={guest.isConfirmed}
-    isEditing={guest.isEditing}
-    handleConfirmation={() => props.toggleConfirmationAt(index)}
-    handleEditing={() => props.toggleEditingAt(index)}
-    setName={text => props.setNameAt(text, index)}
-    handleRemove={() => props.removeGuestAt(index) }
-  />
-)}
-</ul>
+	<ul>
+		{/* Set the PendingGuest Component */}
+		<PendingGuest name={props.pendingGuest}/>
 
+		{/* Filter and display the Guests */}
+		{props.guests
+			.filter(guest => !props.isFiltered || guest.isConfirmed)
+			.map((guest, index) =>
+				<Guest 
+					key={index}
+					name={guest.name}
+					isConfirmed={guest.isConfirmed}
+					isEditing={guest.isEditing}
+					handleConfirmation={() => props.toggleConfirmationAt(index)}
+					handleEditing={() => props.toggleEditingAt(index)}
+					setName={text => props.setNameAt(text, index)}
+					handleRemove={() => props.removeGuestAt(index) }
+				/>
+			)
+		}
+	</ul>
+
+// Set the GuestList Prop Types
 GuestList.propTypes = {
     guests: PropTypes.array.isRequired,
     toggleConfirmationAt: PropTypes.func.isRequired,
@@ -32,4 +50,5 @@ GuestList.propTypes = {
     pendingGuest: PropTypes.string.isRequired,
 };
 
+// Export the GuestList
 export default GuestList;
